1d0e865c0e8031e194958d4d08f3a02ef1dd574a
Vincent Mussa <v.mussa@alustudent.com>
2026-07-27 21:58:30 +0400

    Setup of everything and checking a print statement if it works.

---
+++ main.py
@@ -0,0 +1,5 @@
+#My first real python assignment
+#Student Grade Tracking system
+#Vincent Mussa(v.mussa@alustudent.com)-Computer Science Student(O'25)
+
+print("Student Grade Tracking system first step.")
