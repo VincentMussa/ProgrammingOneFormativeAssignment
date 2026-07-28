07e184d8a89c061da93496880fbb627ac4a128cd
Vincent Mussa <v.mussa@alustudent.com>
2026-07-28 20:36:55 +0400

    Created menu function with input so user can choose from the  menu list.

--- main.py
+++ main.py
@@ -11,4 +11,20 @@ print("2-Enter exam")
 print("3-List assignments")
 print("4-Filter by subject, type or month")
 print("5-Show grade summary")
-print("6-Close")
+print("0-Close")
+
+#Creating a function that has the menu and uses input to ask the user to make selection from the mrenu list
+def menu_list():
+    print(" The following is a list of menu options you are to choose from:")
+    print("1-Enter homework")
+    print("2-Enter exam")
+    print("3-List assignments")
+    print("4-Filter by subject, type or month")
+    print("5-Show grade summary")
+    print("0-Close")
+    selection = input("Choose your option from the menu list above:")
+    return selection
+#Till here the function is working but needs to be called in order to run it.
+
+
+
