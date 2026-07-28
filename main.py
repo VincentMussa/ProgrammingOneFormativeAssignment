98cfe45cb454fddba4049e7a781515a7c8ac1901
Vincent Mussa <v.mussa@alustudent.com>
2026-07-28 20:03:47 +0400

    Created a menu list using print statement.

--- main.py
+++ main.py
@@ -3,3 +3,12 @@
 #Vincent Mussa(v.mussa@alustudent.com)-Computer Science Student(O'25)
 
 print("Student Grade Tracking system first step.")
+
+#Creating a menu list in which users can choose from using print statements
+print(" The following is a list of menu options you are to choose from:")
+print("1-Enter homework")
+print("2-Enter exam")
+print("3-List assignments")
+print("4-Filter by subject, type or month")
+print("5-Show grade summary")
+print("6-Close")
