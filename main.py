abf4a26cdbce0af3dbdc8e010ded3d5b8aad1e48
Vincent Mussa <v.mussa@alustudent.com>
2026-08-01 15:26:23 +0400

    Tried to ways to work with menu choices, first with menu list as a function containing print statements for the options, then separate functions for each menu option.

--- main.py
+++ main.py
@@ -4,16 +4,7 @@
 
 print("Student Grade Tracking system first step.")
 
-#Creating a menu list in which users can choose from using print statements
-print(" The following is a list of menu options you are to choose from:")
-print("1-Enter homework")
-print("2-Enter exam")
-print("3-List assignments")
-print("4-Filter by subject, type or month")
-print("5-Show grade summary")
-print("0-Close")
-
-#Creating a function that has the menu and uses input to ask the user to make selection from the mrenu list
+#Creating a function that has the menu and uses input to ask the user to make selection from the menu list
 def menu_list():
     print(" The following is a list of menu options you are to choose from:")
     print("1-Enter homework")
@@ -24,12 +15,33 @@ def menu_list():
     print("0-Close")
     selection = input("Choose your option from the menu list above:")
     return selection
+
 #Till here the function is working but needs to be called in order to run it.
 user_choice = menu_list()
-print("You have choosen the option:", user_choice)
+
+#Creating if/else functions to make user if selects 5, it gives Show grade summary
+if user_choice == "1":
+    print("This selection allows you to enter your homework assignments")
+
+elif user_choice == "2":
+    print("This selection allows you to enter your exam assignments")
+
+elif user_choice == "3":
+    print("This selection displays all your assignments") 
+
+elif user_choice == "4":
+    print("This selection allows you to filter your assignments by subject, type or month")
+
+elif user_choice == "5":
+        print("This selection allows you to see your grade summary")
+
+elif user_choice == "0":
+    print("This selection closes the program")
+
+else:
+    print("Your choice is not among the list! Please try again")
 
 #Creating function for each menu option as in the future each option can have multiple entries.
-print(" The following is a list of menu options you are to choose from:")
 def Enter_homework():
     print("This selection allows you to enter your homework assignments")
 
@@ -37,17 +49,57 @@ def Enter_exam():
     print("This selection allows you to enter your exam assignments")
 
 def List_assignments():
-    print("This selection allows you to list all your assignments") 
+    print("This selection displays all your assignments") 
 
 def Filter():
     print("This selection allows you to filter your assignments by subject, type or month")
 
-def Grade_Summary():
-    print("This selection allows you to show your grade summary")
+def Grade_summary():
+        print("This selection allows you to see your grade summary")
 
 def Close():
     print("This selection closes the program")
 
+#Creating a function that has the menu and uses input to ask the user to make selection from the mrenu list
+def menu_list():
+    print(" The following is a list of menu options you are to choose from:")
+    print("1-Enter homework")
+    print("2-Enter exam")
+    print("3-List assignments")
+    print("4-Filter by subject, type or month")
+    print("5-Show grade summary")
+    print("0-Close")
+    selection = input("Choose your option from the menu list above: ")
+    return selection
 
+#Till here the function is working but needs to be called in order to run it.
+user_choice = menu_list()
+
+#Adding if/else functions to make user if selects 5, it gives Show grade summary
+if user_choice == "1":
+    Enter_homework()
+
+elif user_choice == "2":
+    Enter_exam()
+
+elif user_choice == "3":
+    List_assignments()
+
+elif user_choice == "4":
+    Filter()
+
+elif user_choice == "5":
+    Grade_summary()
+
+elif user_choice == "0":
+    Close()
+
+else:
+    print("Your choice is not among the list! Please try again")
+
+#Codes above follow the input part that printed "You have chosen the option: 1", it lacked the the if/else function
+#The first codes to line 42, created one function for the menu list with the input and then created if/else functions to make user if selects 5, it gives Show grade summary
+#The second codes from line 44, created function for each menu option as in the future each option can have multiple entries. Then created a function that has the menu and uses input to ask the user to make selection from the menu list. Then added if/else functions to make user if selects 5, it gives Show grade summary
+#The results of the codes are the same, but the second codes are more organized, easier to read and could be used in the future if each menu option has other entries in it
 
 
