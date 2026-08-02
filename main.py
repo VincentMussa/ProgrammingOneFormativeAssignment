9c5c91020c334210d78af1f24b302cfbca756991
Vincent Mussa <v.mussa@alustudent.com>
2026-08-02 09:47:04 +0400

    Added assignment list that is empty and while loop that made the menu list run until user picks 0 to close. I tested with number 1,2,3,4,5,6,7,8,9,0 and with 0 at the end menu list didn't show again.

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 
 print("Student Grade Tracking system first step.")
 
+#Creating empty list where later will have assignments stored
+#assignments like Programming I, Communication for Impact, Projects will be added in the list
+assignments = []
+
 #Creating function for each menu option as in the future each option can have multiple entries.
 def Enter_homework():
     print("This selection allows you to enter your homework assignments")
@@ -35,28 +39,35 @@ def menu_list():
     selection = input("Choose your option from the menu list above: ")
     return selection
 
+#Adding while loop to make the menu option run untill the user chooses to close when inputs 0
+#The menu will show again and again until user closes program
+#A note;The indentation changes because while loop will contain the if/else functions inside it
+keep_running = True
 #Till here the function is working but needs to be called in order to run it.
-user_choice = menu_list()
+while keep_running:
+    user_choice = menu_list()
 
 #Adding if/else functions to make user if selects 5, it gives Show grade summary
-if user_choice == "1":
-    Enter_homework()
+    if user_choice == "1":
+        Enter_homework()
 
-elif user_choice == "2":
-    Enter_exam()
+    elif user_choice == "2":
+        Enter_exam()
 
-elif user_choice == "3":
-    List_assignments()
+    elif user_choice == "3":
+        List_assignments()
 
-elif user_choice == "4":
-    Filter()
+    elif user_choice == "4":
+        Filter()
 
-elif user_choice == "5":
-    Grade_summary()
+    elif user_choice == "5":
+        Grade_summary()
 
-elif user_choice == "0":
-    Close()
+    elif user_choice == "0":
+        Close()
+        keep_running = False
+#Here the menu list stops option if the user chooses to close
 
-else:
-    print("Your choice is not among the list! Please try again")
+    else:
+        print("Your choice is not among the list! Please try again")
 #Deleted the first codes, and kept the functions for each menu option, so that in the future each option can have multiple entries.
